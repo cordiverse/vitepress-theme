b6ce45ad9aecdb336ef4d983aabfdbeada3c6432
Shigma <shigma10826@gmail.com>
2022-11-01 00:44:23 +0800

    feat: setup client entry

---
+++ client/index.ts
@@ -0,0 +1,28 @@
+import { Theme } from 'vitepress'
+import { Component, InjectionKey } from 'vue'
+import VPDoc from '@theme-default/components/VPDoc.vue'
+import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
+import './styles/aside.scss'
+import './styles/code.scss'
+import './styles/doc.scss'
+import './styles/vars.scss'
+
+export interface ClientConfig {
+  layouts?: Record<string, Component>
+}
+
+export const ClientConfig: InjectionKey<ClientConfig> = Symbol('config')
+
+export const defineTheme = (config: ClientConfig = {}): Theme => ({
+  ...DefaultTheme,
+  Layout,
+  enhanceApp({ app }) {
+    app.provide(ClientConfig, {
+      layouts: {
+        ...config.layouts,
+        default: VPDoc,
+      },
+    })
+  },
+})
